f7c54775c718cc852e146fb682ceb8b2a40edc39
Vinicius Borille da Silva <viniciusborilledasilva@gmail.com>
2026-04-28 20:52:22 -0300

    refactor: ensuring token validity

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -13,7 +13,7 @@ async function getHandler(request, response) {
     const sessionToken = request.cookies.session_id;
 
     const sessionObject = await session.findOneValidByToken(sessionToken)
-
+  
     const userFound = await user.findOneById(sessionObject.user_id);
     return response.status(200).json(userFound);
 }
--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -1,10 +1,11 @@
 import orchestrator from "tests/orchestrator"
 import { version as uuidVersion } from "uuid";
+import session from "models/session.js";
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices();
-  await orchestrator.clearDatabase();
-  await orchestrator.runPendingMigrations();
+    await orchestrator.waitForAllServices();
+    await orchestrator.clearDatabase();
+    await orchestrator.runPendingMigrations();
 });
 
 describe("GET /api/v1/user", () => {
@@ -15,7 +16,7 @@ describe("GET /api/v1/user", () => {
             });
 
             const sessionObject = await orchestrator.createSession(createdUser.id)
-            
+
             const response = await fetch("http://localhost:3000/api/v1/user", {
                 headers: {
                     Cookie: `session_id=${sessionObject.token}`
@@ -39,6 +40,56 @@ describe("GET /api/v1/user", () => {
             expect(uuidVersion(responseBody.id)).toBe(4);
             expect(Date.parse(responseBody.created_at)).not.toBeNaN();
             expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+        });
+        test("With nonexistent session", async () => {
+            const nonexistentToken = " 5575f9a11eddcc674f50cbd1492fdc8cff2966f37c093d4acbca670ad182f7a8e014e51ccb09691ce666e8de8b812ad5";
+
+            const response = await fetch("http://localhost:3000/api/v1/user", {
+                headers: {
+                    Cookie: `session_id=${nonexistentToken}`
+                }
+            });
+
+            expect(response.status).toBe(401);
+
+            const responseBody = await response.json();
+
+            expect(responseBody).toEqual({
+                name: "UnauthorizedError",
+                message: "Usuário não possui sessão ativa.",
+                action: "Verifique se este usuário está logado e tente novamente.",
+                status_code: 401,
+            });
+        });
+        test("With expired session", async () => {
+            jest.useFakeTimers({
+                now: new Date(Date.now() - session.EXPIRATION_IN_MILISECONDS)
+            });
+
+            const createdUser = await orchestrator.createUser({
+                username: "UserWithExpiredSession",
+            });
+
+            const sessionObject = await orchestrator.createSession(createdUser.id)
+
+            jest.useRealTimers();
+
+            const response = await fetch("http://localhost:3000/api/v1/user", {
+                headers: {
+                    Cookie: `session_id=${sessionObject.token}`
+                }
+            });
+
+            expect(response.status).toBe(401);
+
+            const responseBody = await response.json();
+
+            expect(responseBody).toEqual({
+                name: "UnauthorizedError",
+                message: "Usuário não possui sessão ativa.",
+                action: "Verifique se este usuário está logado e tente novamente.",
+                status_code: 401,
+            });
         });
     });
 });
