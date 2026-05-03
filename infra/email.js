fdea1842a8c2ced4b7f55243121c5c4297d2d73d
Vinicius Borille da Silva <viniciusborilledasilva@gmail.com>
2026-05-03 09:47:27 -0300

    feat: add `mailcatcher` to local services

--- infra/email.js
+++ infra/email.js
@@ -1,21 +1,21 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_SMTP_HOST,
-    port: process.env.EMAIL_SMTP_PORT,
-    auth: {
-        user: process.env.EMAIL_SMTP_USER,
-        pass: process.env.EMAIL_SMTP_PASSWORD,
-    },
-    secure: process.env.NODE_ENV === "production" ? true : false,
-})
+  host: process.env.EMAIL_SMTP_HOST,
+  port: process.env.EMAIL_SMTP_PORT,
+  auth: {
+    user: process.env.EMAIL_SMTP_USER,
+    pass: process.env.EMAIL_SMTP_PASSWORD,
+  },
+  secure: process.env.NODE_ENV === "production" ? true : false,
+});
 
 async function send(mailOptions) {
-    await transporter.sendMail(mailOptions)
+  await transporter.sendMail(mailOptions);
 }
 
 const email = {
-    send,
-}
+  send,
+};
 
 export default email;
